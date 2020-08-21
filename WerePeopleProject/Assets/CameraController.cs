e993f685867cfa4e46326054fe897700ec06e352
jlzeng-i-got-locked-out-of-rokky258 <56747635+jlzeng-i-got-locked-out-of-rokky258@users.noreply.github.com>
2020-08-21 18:57:39 -0400

    Camera
    aaaa

--- WerePeopleProject/Assets/CameraController.cs
+++ WerePeopleProject/Assets/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public float rotationSens;
     // Start is called before the first frame update
 
     void Start()
@@ -15,7 +16,23 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+         if (Input.GetKey(KeyCode.UpArrow) && !(transform.position.y > 7.6))
+        {
+            transform.Translate(Vector3.up * rotationSens);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && !(transform.position.y < 1))
+        {
+            transform.Translate(Vector3.down * rotationSens);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && !(transform.position.z > 0 && transform.position.x > 0)){
+            transform.Translate(Vector3.right * rotationSens);
+            Debug.Log(transform.position);
+        }
+        
+          if (Input.GetKey(KeyCode.LeftArrow) && !(transform.position.z > 0 && transform.position.x < 0)){
+            transform.Translate(Vector3.left * rotationSens);
+            Debug.Log(transform.position);
+        }
         transform.LookAt(target);
     }
 }
