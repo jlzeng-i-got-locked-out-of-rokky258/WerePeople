6843bbf4deebbb8fb59032955022b50546e87cce
jlzeng-i-got-locked-out-of-rokky258 <56747635+jlzeng-i-got-locked-out-of-rokky258@users.noreply.github.com>
2020-08-22 12:12:48 -0400

    Made it easier to initialize the Unit + Weapon + whatever else later

---
+++ WerePeopleProject/Assets/UnitInitial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitInitial : MonoBehaviour
+{  public GameObject unit, weapon;
+    // Start is called before the first frame update
+    public Sprite unitSprite, weaponSprite;
+    
+    public Color wColor;
+    void Start()
+    {
+        unit.GetComponent<SpriteRenderer>().sprite = unitSprite;
+        weapon.GetComponent<SpriteRenderer>().sprite = weaponSprite;
+        weapon.GetComponent<SpriteRenderer>().color = wColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
