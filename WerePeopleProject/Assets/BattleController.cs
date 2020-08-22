0c40f988b288fbaaad4dd75b60daf6130438ca53
Adam Powell <apowell@umass.edu>
2020-08-21 22:53:48 -0400

    Initial combat cutscene

---
+++ WerePeopleProject/Assets/BattleController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour
+{
+
+    public CombatantAnimationParams leftCombat;
+    public CombatantAnimationParams rightCombat;
+    private int combatTime = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Disable();
+    }
+
+    void Disable()
+    {
+        combatTime = 1000;
+        gameObject.GetComponent<Camera>().enabled = false;
+    }
+
+    void Enable()
+    {
+        combatTime = 0;
+        gameObject.GetComponent<Camera>().enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Enable();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        combatTime++;
+
+        if (combatTime == 10)
+        {
+            leftCombat.Attack();
+        }
+
+        if (combatTime == 65)
+        {
+            rightCombat.Attack();
+        }
+
+        if (combatTime == 140)
+        {
+            Disable();
+        }
+    }
+}
---
+++ WerePeopleProject/Assets/CombatantAnimationParams.cs
@@ -0,0 +1,33 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class CombatantAnimationParams : MonoBehaviour
+{
+
+    public float horizontalOffset;
+    public float verticalOffset;
+    public float shakeAmount;
+    public float orientation;
+    public Vector3 offset;
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = offset + new Vector3(horizontalOffset * orientation + Random.Range(-shakeAmount, shakeAmount) / 100, verticalOffset + Random.Range(-shakeAmount, shakeAmount) / 100, 1);
+        
+    }
+
+    public void Attack()
+    {
+        anim.SetTrigger(Animator.StringToHash("BasicAttack"));
+    }
+}
